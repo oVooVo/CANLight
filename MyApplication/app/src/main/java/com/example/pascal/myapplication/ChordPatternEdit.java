a446d735319fd990010f69ae9d6b8adb6cd7a009
Pascal Bies <pascal.bies.42@gmail.com>
2016-10-04 01:13:32 +0200

    solved performance problems

--- MyApplication/app/src/main/java/com/example/pascal/myapplication/ChordPatternEdit.java
+++ MyApplication/app/src/main/java/com/example/pascal/myapplication/ChordPatternEdit.java
@@ -2,6 +2,7 @@ package com.example.pascal.myapplication;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.Spannable;
 import android.text.Spanned;
@@ -10,68 +11,61 @@ import android.text.style.StyleSpan;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by pascal on 03.10.16.
  */
 public class ChordPatternEdit extends EditText {
 
-    public ChordPatternEdit(Context context, AttributeSet attrs) {
+    private Handler updateHighlightsHandler;
+
+    public ChordPatternEdit(final Context context, AttributeSet attrs) {
         super(context, attrs);
         setGravity(Gravity.LEFT | Gravity.TOP);
+
         addTextChangedListener(new TextWatcher() {
-
-
-            class Lines {
-                String[] lines;
-                int offset;
-            }
-
-            Lines getLines(String s, int start, int length) {
-                String[] lines = s.split("\n", -1);
-                ArrayList<String> linesOfInterest = new ArrayList<>();
-
-                final int end = start + length - 1;
-
-                int position = 0;
-                int offset = -1;
-                for (String line : lines) {
-                    final int lineStart = position;
-                    final int lineEnd = position + line.length();
-
-                    if ( (start <= lineStart + 1 && end >= lineStart)
-                      || (start >= lineStart - 1 && start <= lineEnd) ) {
-                        if (offset < 0) {
-                            offset = lineStart;
-                        }
-                        linesOfInterest.add(line);
-                    }
-
-                    position = lineEnd + 1;
-                }
-
-                Lines result = new Lines();
-                result.lines = linesOfInterest.toArray(new String[linesOfInterest.size()]);
-                result.offset = offset;
-                return result;
-            }
-
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                for (StyleSpan ss : getText().getSpans(start, start + count - 1, StyleSpan.class)) {
+                for (StyleSpan ss : getText().getSpans(0, getText().length() - 1, StyleSpan.class)) {
                     getText().removeSpan(ss);
                 }
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (updateHighlightsHandler != null) {
+                    System.out.println("Cancel timer");
+                    updateHighlightsHandler.removeCallbacksAndMessages(null);
+                }
+                updateHighlightsHandler = new Handler();
+                updateHighlightsHandler.postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        updateHighlights();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //updateHighlights();
+            }
+
+            private void updateHighlights() {
                 Spannable span = getText();
-                Lines lines = getLines(span.toString(), start, count);
-                int position = lines.offset;
-                for (String line : lines.lines) {
+                String text = getText().toString();
+                String[] lines = text.split("\n");
+
+                int position = 0;
+                for (String line : lines) {
                     Chord.Line cLine = Chord.parseLine(line);
                     for (String token : cLine.tokens) {
                         Chord chord = Chord.chordFromString(token);
@@ -83,30 +77,6 @@ public class ChordPatternEdit extends EditText {
                     }
                 }
             }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                //updateHighlights();
-            }
         });
-    }
-
-    private void updateHighlights() {
-        Spannable span = getText();
-        String text = getText().toString();
-        String[] lines = text.split("\n");
-
-        int position = 0;
-        for (String line : lines) {
-            Chord.Line cLine = Chord.parseLine(line);
-            for (String token : cLine.tokens) {
-                Chord chord = Chord.chordFromString(token);
-                if (cLine.isChordLine && chord.isValid()) {
-                    StyleSpan bold = new StyleSpan(Typeface.BOLD);
-                    span.setSpan(bold, position, position + token.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                }
-                position = position + token.length() + 1;
-            }
-        }
     }
 }
